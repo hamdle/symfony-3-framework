71b5b86c8651bc0d0ff7611ad3c445123f245cb3
hamdle <contact@ericmarty.co>
2018-05-21 14:54:05 -0700

    Associate controller with each route

--- web/front.php
+++ web/front.php
@@ -6,6 +6,19 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+
+    // Debug
+    var_dump($request->attributes->all());
+
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
@@ -15,7 +28,7 @@ $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
     $request->attributes->add($matcher->match($request->getPathInfo()));
-    $response = call_user_func('render_template', $request);
+    $response = call_user_func($request->attributes->get("_controller"), $request);
 } catch (Routing\Exception\ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
 } catch (Exception $exception) {
@@ -23,12 +36,3 @@ try {
 }
 
 $response->send();
-
-function render_template($request)
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-    return new Response(ob_get_clean());
-}
