8359f579c9a9c4129121a1ab30f7030bd3bea7d7
hamdle <contact@ericmarty.co>
2018-05-21 10:22:22 -0700

    Add render_template function to front loader

--- web/front.php
+++ web/front.php
@@ -23,3 +23,12 @@ try {
 }
 
 $response->send();
+
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
