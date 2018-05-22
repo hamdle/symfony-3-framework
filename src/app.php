f2c0231fc927cfe71354930b44fc52c954b13024
hamdle <contact@ericmarty.co>
2018-05-21 17:29:30 -0700

    Trying my own version of is_leap_year page

--- src/app.php
+++ src/app.php
@@ -1,6 +1,11 @@
 <?php
 // framework/src/app.php
 use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
+
+function is_leap_year($request) {
+    return new Response('False');
+}
 
 $routes = new Routing\RouteCollection();
 
@@ -13,6 +18,12 @@ $routes->add('hello', new Routing\Route('/hello/{name}', array(
 $routes->add('bye', new Routing\Route('/bye', array(
     '_controller' => function ($request) {
         return render_template($request);
+    }
+)));
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+    'year' => '2018',
+    '_controller' => function ($request) {
+        return is_leap_year($request);
     }
 )));
 
