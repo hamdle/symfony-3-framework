e6b730c6bf97b44bfdfca46de42d18b2c6c043a6
hamdle <contact@ericmarty.co>
2018-05-22 14:51:33 -0700

    Convert leap year controller to a class

--- src/app.php
+++ src/app.php
@@ -3,6 +3,17 @@
 use Symfony\Component\Routing;
 use Symfony\Component\HttpFoundation\Response;
 
+class LeapYearController
+{
+    public function indexAction($request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year!');
+        }
+
+        return new Response('Nope, this is not a leap year.');
+    }
+}
+
 function is_leap_year($year = null) {
    if (null === $year) {
        $year = date('Y');
@@ -26,16 +37,7 @@ $routes->add('bye', new Routing\Route('/bye', array(
 )));
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => function ($request) {
-        // Debug
-        var_dump($request->attributes->all());
-
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response('Yep, this is a leap year!');
-        }
-
-        return new Response('Nope, this is not a leap year.');
-    }
+    '_controller' => array(new LeapYearController(), 'indexAction')
 )));
 
 return $routes;
